fc19878ba6ba70fc0f5df8cd5542ac30603113c1
David Antliff <david.antliff@aviatnet.com>
2017-05-25 14:20:38 +1200

    Add variant of keyword-only arguments.

--- keyword_only_arguments.py
+++ keyword_only_arguments.py
@@ -4,11 +4,29 @@ def f(a, b, *c, d=None):
     print(f"a {a}, b {b}, c {c}, d {d}")
 
 
+def g(a, b, *, c, d=None):
+    """c is mandatory!"""
+    print(f"a {a}, b {b}, c {c}, d {d}")
+
+
 def main():
     f(1, 2)          # a 1, b 2, c (), d None
     f(1, 2, 3)       # a 1, b 2, c (3,), d None
     f(1, 2, 3, 4)    # a 1, b 2, c (3, 4), d None
     f(1, 2, 3, d=4)  # a 1, b 2, c (3,), d 4
+
+    try:
+        g(1, 2)          # g() missing 1 required keyword-only argument: 'c'
+    except Exception as e:
+        print(str(e))
+
+    try:
+        g(1, 2, 3)       # g() takes 2 positional arguments but 3 were given
+    except Exception as e:
+        print(str(e))
+
+    g(1, 2, c=3)       # a 1, b 2, c 3, d None
+    g(1, 2, c=3, d=4)  # a 1, b 2, c 3, d 4
 
 
 if __name__ == "__main__":
